b9cd376350895a87706894bd7c416c8cfb7ff200
nicoorecke <recke.nico@gmail.com>
2022-10-25 23:27:29 -0300

    AAA

--- src/pages/Modulos.js
+++ src/pages/Modulos.js
@@ -2,165 +2,36 @@ import getHash from "../utils/getHash";
 import getData from "../utils/getData";
 
 const Modulos= async () => {
-  const id = getHash();
+  
   const character = await getData(0);
-  const superr =  character.map(function(element){
-    console.log(element, 'kaka')
-    return `<p>${element.precio}</p>
-            <div>${element.potencia}</div>`;
-  })
 
+  const superr = `<section class="Introduction">
+      ${character.map(element => `
+        <div class="image">
+            <a href="https://enertik.ar/panel-solar-fotovoltaico-policristalino-310w">
+                <img src=${element.imagen} alt=""> 
+            </a>
+        </div>
+
+        <div class="Datos">
+            <p>${element.marca}</p>
+            <p class="precio">${element.precio}</p>
+            <p class="potencia">${element.potencia}</p>
+        </div>
+
+      `).join('')}
+      </section>
+  `
 
     const view = `
     <h2> Módulos</h2>
-    <div>${superr} </div>
+    
 <div class="image-top">
         <img class="imagen" src="https://enertik.ar/images/schema/enertik.png" alt="">
     </div>
-<section class="Introduction">
-    
-    <div class="image">
-        <a href="https://enertik.ar/panel-solar-fotovoltaico-policristalino-310w">
-            <img src="https://enertik.ar/images/productos/ps/ps260.webp" alt=""> 
-        </a>
-    </div>
-    
-    <div class="Datos">
-        <img src="https://enertik.ar/images/schema/enertik.png" alt=""> 
-        <p class="precio">$69.237</p>
-        <p class="potencia">310W</p>
-    </div>
-
-    <div class="image">
-        <a href="https://enertik.ar/panel-solar-fotovoltaico-policristalino-340w">
-            <img src="https://enertik.ar/images/productos/ps/ps310.png" alt=""> 
-        </a>
-    </div>
+    <div>${superr}</div>
     
 
-    <div class="Datos">
-        <img src="https://enertik.ar/images/schema/enertik.png" alt=""> 
-        <p class="precio">$76.851</p>
-        <p class="potencia">340W</p>
-    </div>
-
-    <div class="image">
-        <a href="https://enertik.ar/panel-solar-fotovoltaico-policristalino-360w">
-            <img src="https://enertik.ar/images/productos/ps/ps310.png" alt=""> 
-        </a>
-    </div>
-
-    <div class="Datos">
-        <img src="https://enertik.ar/images/schema/enertik.png" alt=""> 
-        <p class="precio">$81.455</p>
-        <p class="potencia">360W</p>
-    </div>
-    
-    <div class="image">
-        <a href="https://enertik.ar/trina-solar-tsm-375de08m-ii-honey-panel-solar-fotovoltaico-monocristalino-375w">
-            <img src="https://enertik.ar/images/productos/trinasolar/tsm-370de08m-ii.webp" alt="">
-        </a>
-    </div>
-
-    <div class="Datos">
-        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/52/Trina_Solar_logo.svg/2560px-Trina_Solar_logo.svg.png" alt=""> 
-        <p class="precio">$80.570</p>
-        <p class="potencia">375W</p>
-    </div>
-
-    <div class="image">
-        <a href="https://enertik.ar/trina-solar-tsm-450de17m-ii-tallmax-panel-solar-fotovoltaico-monocristalino-450w">
-            <img src="https://enertik.ar/images/productos/trinasolar/tsm-370de08m-ii.webp" alt="">
-        </a>
-    </div>
-    
-
-    <div class="Datos">
-        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/52/Trina_Solar_logo.svg/2560px-Trina_Solar_logo.svg.png" alt="">
-        <p class="precio">$104.652</p>
-        <p class="potencia">450W</p>
-    </div>
-
-    <div class="image">
-        <a href="https://enertik.ar/trina-solar-tsm-550de19-vertex-panel-solar-fotovoltaico-monocristalino-550w">
-            <img src="https://enertik.ar/images/productos/trinasolar/tsm-495de18m-ii.webp" alt="">
-        </a>
-    </div>
-    
-
-    <div class="Datos">
-        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/52/Trina_Solar_logo.svg/2560px-Trina_Solar_logo.svg.png" alt="">
-        <p class="precio">$130.859</p>
-        <p class="potencia">550W</p>
-    </div>
-
-    <div class="image">
-        <a href="https://enertik.ar/amerisolar-as-6p30-285w-panel-solar-fotovoltaico-policristalino-285w"></a>
-            <img src="https://enertik.ar/images/productos/ams/as-6p30-285w.webp" alt="">
-        </a>
-    </div>
-    
-
-    <div class="Datos">
-        <img src="https://www.weamerisolar.eu/wp-content/uploads/2019/04/amerisolar-logo-1500p.png" alt=""> 
-        <p class="precio">$64.279</p>
-        <p class="potencia">285W</p>
-    </div>
-
-    <div class="image">
-        <a href="https://enertik.ar/amerisolar-as-6p-345w-panel-solar-fotovoltaico-policristalino-345w">
-            <img src="https://enertik.ar/images/productos/ams/as-6p30-285w.webp" alt="">
-        </a>
-    </div>
-    
-
-    <div class="Datos">
-        <img src="https://www.weamerisolar.eu/wp-content/uploads/2019/04/amerisolar-logo-1500p.png" alt=""> 
-        <p class="precio">$77.914</p>
-        <p class="potencia">345W</p>
-    </div>
-
-    <div class="image">
-        <a href="https://enertik.ar/amerisolar-as-6m144-hc-450w-panel-solar-fotovoltaico-monocristalino-450w">   
-            <img src="https://enertik.ar/images/productos/ams/as-6m144-hc-450w.webp" alt="">
-        </a>
-    </div>
-    
-
-    <div class="Datos">
-        <img src="https://www.weamerisolar.eu/wp-content/uploads/2019/04/amerisolar-logo-1500p.png" alt="">
-        <p class="precio">$112.089</p>
-        <p class="potencia">450W</p>
-    </div>
-
-    <div class="image">
-    <a href="https://enertik.ar/restarsolar-rt7i-460m-panel-solar-fotovoltaico-monocristalino-460w">
-        <img src="https://enertik.ar/images/productos/restarsolar/rt7i-460m.webp" alt="">
-    </a>
-    </div>
-    
-
-    <div class="Datos">
-        <img src="https://www.resunsolar.com/wp-content/uploads/2021/06/restar-solar.png" alt=""> 
-        <p class="precio">$99.694</p>
-        <p class="potencia">460W</p>
-    </div>
-
-    <div class="image">
-    <a href="https://enertik.ar/restarsolar-rt8i-560m-panel-solar-fotovoltaico-monocristalino-560w">
-        <img src="https://enertik.ar/images/productos/restarsolar/rt7i-460m.webp" alt="">
-    </a>
-    </div>
-    
-
-    <div class="Datos">
-        <img src="https://www.resunsolar.com/wp-content/uploads/2021/06/restar-solar.png" alt="">
-        <p class="precio">$125.193</p>
-        <p class="potencia">560W</p>
-    </div>
-    
-</section>
-        
         
     `;
     return view;
--- src/utils/getData.js
+++ src/utils/getData.js
@@ -3,6 +3,8 @@ import axios from 'axios';
 const enertik = ['paneles-solares','cables-fotovoltaicos','accesorios-y-protecciones']
 
 const BASE_URL = 'https://api.scraperapi.com?api_key=60ae15c4e20edcc002f97d648fc6e399&url=https://enertik.ar/'
+const enertikbase = 'https://enertik.ar/'
+
 
 const getData = async (id) => {
   try {
@@ -11,15 +13,36 @@ const getData = async (id) => {
     const datos = response.data;
     var doc = parser.parseFromString(datos, 'text/html');
     if (id==0){
-      
-      let powerArr = Array.from(doc.querySelector('#productGridPage ul').querySelectorAll('.backLI p:first-of-type'))
-      //console.log(powerArr)
-      let pricesArr = Array.from(doc.querySelector('#productGridPage ul').querySelectorAll('.priceMenu'))
-      //console.log(pricesArr)
+      let panel = doc.querySelector('#productGridPage ul')
 
+
+      let imgArr = Array.from(panel.querySelectorAll('.frontLI'))
+      console.log(imgArr)
+      let powerArr = Array.from(panel.querySelectorAll('.backLI p:first-of-type'))
+      //console.log(powerArr)
+      let pricesArr = Array.from(panel.querySelectorAll('.priceMenu'))
+      //console.log(pricesArr)
+      let marca
       let listapiola = pricesArr.map(function(element, i){
-        console.log(i)
-        return {'precio' : element.innerText, 'potencia' : powerArr[i].innerText}
+        if (imgArr[i].querySelector('.blueTopTag') ) {
+          marca = imgArr[i].querySelector('.blueTopTag').innerText
+        }
+        else  if (imgArr[i].querySelector('.orangeTopTag')){
+          marca = imgArr[i].querySelector('.orangeTopTag').innerText
+        }
+
+        else  if (imgArr[i].querySelector('.greyTopTag')){
+          marca = imgArr[i].querySelector('.greyTopTag').innerText
+        }
+
+        else {
+          marca = 'Enertik' 
+        }
+        return {
+          'imagen' : enertikbase + imgArr[i].querySelector('picture img').dataset.src,
+          'marca' : marca,
+          'precio' : element.innerText,
+          'potencia' : powerArr[i].innerText}
       })
 
       console.log(listapiola);
